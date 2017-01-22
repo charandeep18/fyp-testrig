1053ace8f239a733915aea672c39e293f623b6e4
Charandeep Rai <charandeep.rai@gmail.com>
2017-01-22 14:43:13 +0000

    Added some sample Cucumber Scripts
    Added additional cucumber scripts and scenarios

--- src/test/java/testcases/Openwebpage.java
+++ src/test/java/testcases/Openwebpage.java
@@ -2,6 +2,9 @@ package testcases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.*;
+
+import cucumber.annotation.en.And;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
@@ -10,8 +13,19 @@ public class Openwebpage {
 WebDriver driver = null;
 @Given("^user navigates to Website$")
 public void goToSite() {
-System.setProperty("webdriver.gecko.driver", "C:\\Users\\chara\\Desktop\\geckodriver.exe");
-driver = new FirefoxDriver();
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\chara\\Desktop\\chromedriver.exe");
+driver = new ChromeDriver();
 driver.navigate().to("http://127.0.0.1:56039/Homepage.html");
 }
+
+@And("^find the link to the webpage$")
+public void findSite2(){
+driver.findElement(By.id("link1"));
+}
+
+@Then("^click on the link$")
+public void clickSite2(){
+driver.findElement(By.id("link1")).click();
+}
+
 }
--- target/cucumber/report.js
+++ target/cucumber/report.js
@@ -19,11 +19,72 @@ formatter.step({
   "name": "user navigates to Website",
   "keyword": "Given "
 });
+formatter.step({
+  "line": 5,
+  "name": "find the link to the webpage",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "click on the link",
+  "keyword": "Then "
+});
 formatter.match({
   "location": "Openwebpage.goToSite()"
 });
 formatter.result({
-  "duration": 3563566395,
+  "duration": 3490144992,
   "status": "passed"
+});
+formatter.match({
+  "location": "Openwebpage.findSite2()"
+});
+formatter.result({
+  "duration": 25710607,
+  "status": "passed"
+});
+formatter.match({
+  "location": "Openwebpage.clickSite2()"
+});
+formatter.result({
+  "duration": 816523135,
+  "status": "passed"
+});
+formatter.uri('testcases\Openwebpage2.feature');
+formatter.feature({
+  "line": 1,
+  "name": "Openwebpage",
+  "description": "This is going to be a test to ensure that the selenium webdriver is able to open up the tests as intended.",
+  "id": "openwebpage",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Successfully opening Website.",
+  "description": "",
+  "id": "openwebpage;successfully-opening-website.",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "user navigates to Website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "navigate to Link-1 Homepage",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "Openwebpage.goToSite()"
+});
+formatter.result({
+  "duration": 2795121068,
+  "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
 });
 });
