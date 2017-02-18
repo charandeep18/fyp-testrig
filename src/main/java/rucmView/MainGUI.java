7dae80ae95cca20c4e407b2656e14dc35696f8de
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-18 16:40:14 +0000

    Fixed Issue with Main Class, now working as intended

--- src/main/java/rucmView/MainGUI.java
+++ src/main/java/rucmView/MainGUI.java
@@ -1,5 +1,7 @@
 package rucmView;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,14 +12,20 @@ public class MainGUI extends Application {
 
 	@Override
 	public void start(Stage mainStage) throws Exception {
-		Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
-		mainStage.setTitle("RUCM Application");
-		mainStage.setScene(new Scene(root, 800, 500));
-		mainStage.show();
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("GherkinView.fxml"));
+				mainStage.setTitle("RUCM Application");
+				mainStage.setScene(new Scene(root, 800, 500));
+				mainStage.show();
+		
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
 }
