888c82bac38629faa002a84a0eb58ca9bc003d39
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-18 15:57:01 +0000

    Fixed Issues with JavaFX and Controller class

--- src/main/java/rucmView/GherkinController.java
+++ src/main/java/rucmView/GherkinController.java
@@ -1,17 +1,61 @@
 package rucmView;
 
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import View.GUIMenuBar;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
 
 public class GherkinController {
 
+	private Desktop desktop = Desktop.getDesktop();
+	private final FileChooser fileChooser = new FileChooser();
+	
     @FXML
     private MenuItem exit;
+    
+    @FXML
+    private MenuItem loadFile;
 
     @FXML
-    void ExitApplication(ActionEvent event) {
-
+    public void ExitApplication(ActionEvent event) {
+		Platform.exit();
+		System.exit(0);
+		System.out.println("Program has closed successfully");
+    }
+    
+    @FXML
+    public void openFile(File file) {
+        try {
+            desktop.open(file);
+        } catch (IOException ex) {
+            Logger.getLogger(
+                GUIMenuBar.class.getName()).log(
+                    Level.SEVERE, null, ex
+                );
+        }
+    }
+    
+    @FXML
+    public void saveFile(File file, String content) {
+    	try {
+    		FileWriter fileWriter = null;
+    		fileWriter = new FileWriter(file);
+    		fileWriter.write(content);
+    		fileWriter.close();
+    	} catch (IOException ex) {
+    		Logger.getLogger(
+    				GUIMenuBar.class.getName()).log(
+    						Level.SEVERE, null, ex);
+    	}
     }
 
 }
