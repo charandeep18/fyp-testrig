eb478caaed1895973cf112d719ad80773d48b5c9
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-19 02:15:37 +0000

    Changes made to the OpenFile method
    Changes have been made to the openfile method to get it to open into the
    text area. However it is still not fully working

---
+++ src/main/java/Controller/readFile.java
@@ -0,0 +1,7 @@
+//package Controller;
+//
+//public class readFile {
+//	
+//	public String readFile(File file)
+//	
+//}
--- src/main/java/rucmView/GherkinController.java
+++ src/main/java/rucmView/GherkinController.java
@@ -1,7 +1,10 @@
 package rucmView;
 
 import java.awt.Desktop;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -12,6 +15,7 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuBar;
 import javafx.scene.layout.AnchorPane;
@@ -42,6 +46,11 @@ public class GherkinController {
     @FXML
     private GridPane GridPane;
     
+    @FXML
+    private Button closeButton;
+
+    @FXML
+    private TextArea readTextArea;
    
     @FXML
     public void OpenFile(ActionEvent event) {
@@ -53,7 +62,7 @@ public class GherkinController {
    	
     	File file = filechooser.showOpenDialog(MainStage.getScene().getWindow());
     	if(file != null){
-    		
+    		readTextArea.setText(readFile(file));
     	}
     	
     	
@@ -79,34 +88,33 @@ public class GherkinController {
 		System.out.println("Program has closed successfully");
     }
     
-
+    private String readFile(File file){
+    	//Brings in the BufferedReader IO Library from Java
+    	BufferedReader bufferedReader = null;
+    	//Brings in the StringBuilder Library from Java
+    	StringBuilder stringBuffer = new StringBuilder();
     	
-    	
-    	//            File file = fileChooser.showOpenDialog(new Stage());
-//            fileChooser.setTitle("Open Gherkin File");
-//            if (file != null) {
-//            	try {
-//            		desktop.open(file);
-//            	} catch (IOException exception) {
-//            		Logger.getLogger(
-//            				MainGUI.class.getName()).log(Level.SEVERE, null, exception);
-//            	}
-//            	}
-//            }
-//    
-
-//    @FXML
-//    public void saveFile(File file, String content) {
-//    	try {
-//    		FileWriter fileWriter = null;
-//    		fileWriter = new FileWriter(file);
-//    		fileWriter.write(content);
-//    		fileWriter.close();
-//    	} catch (IOException ex) {
-//    		Logger.getLogger(
-//    				GUIMenuBar.class.getName()).log(
-//    						Level.SEVERE, null, ex);
-//    	}
-//    }
+    	try {
+    		bufferedReader = new BufferedReader(new FileReader(file));
+    		String inputtext;
+    		while((inputtext = bufferedReader.readLine()) !=null){
+    			stringBuffer.append(inputtext);
+    		}
+    	} catch (FileNotFoundException fileexception) {
+    		Logger.getLogger(
+    				GherkinController.class.getName()).log(Level.SEVERE, null, fileexception);
+    	} catch (IOException inputexception) {
+    		Logger.getLogger(
+    				GherkinController.class.getName()).log(Level.SEVERE, null, inputexception);
+    	} finally {
+    		try {
+    			bufferedReader.close();
+    		} catch (IOException inputexception) {
+        		Logger.getLogger(
+        				GherkinController.class.getName()).log(Level.SEVERE, null, inputexception);
+    		}
+    	}    	
+    	return stringBuffer.toString();
+    }
 
 }
