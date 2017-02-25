6b2699ba3c1aadffc468d3b61bc739bd9ead1b16
Charandeep Rai <charandeep.rai@gmail.com>
2017-02-25 15:23:12 +0000

    Code Changes

--- src/main/java/model/checkSyntax.java
+++ src/main/java/model/checkSyntax.java
@@ -1,10 +1,15 @@
 package model;
 
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.LineNumberFactory;
+
 public class checkSyntax {
 
 	public static void checkSyntaxMethod(){
 		
+		final String[] KEYWORDS = new String[] {
+				"Given, Then, And, But, Feature, Scenario, When, Background"
+		};
 	}
-	
 	
 }
